3e102b7e8ab6836aff3c30c53596163e54703926
erisssssss <168049674+erisssssss@users.noreply.github.com>
2024-05-14 22:46:38 +0700

    membuat tabel mahasiswa

---
+++ project1/database/migrations/2024_05_14_154536_mahasiswas.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nim',50)->unique();
+            $table->string('nama',100);
+            $table->string('tempatLahir',100);
+            $table->date('tanggalLahir');
+            $table->string('jenisKelamin',20);
+            $table->string('agama',20);
+            $table->bigInteger('jurusans_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
