1be423dad8ec655675aec7cd0ae3617a2b6b81b8
erisssssss <168049674+erisssssss@users.noreply.github.com>
2024-05-15 22:44:21 +0700

    membuat tabel  mahasiswa

--- project1/routes/web.php
+++ project1/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JurusanController;
-
+use App\Http\Controllers\MahasiswaController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,3 +22,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/jurusan/', [JurusanController::class, 'index']);
+Route::get('/mahasiswa/', [MahasiswaController::class, 'index']);
