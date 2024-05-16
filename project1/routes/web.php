fd9a23fcc52c159b3eac156d2a90ad95258b01ff
erisssssss <168049674+erisssssss@users.noreply.github.com>
2024-05-16 11:22:28 +0700

    tambah jurusan mahasiswa part 2

--- project1/routes/web.php
+++ project1/routes/web.php
@@ -2,24 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JurusanController;
-use App\Http\Controllers\MahasiswaController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('master');
+// });
+
+// Route::get('/tes-git', function () {
+//     return "Halo, ini cuma coba pakai git";
+// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/jurusan/', [JurusanController::class, 'index']);
-Route::get('/mahasiswa/', [MahasiswaController::class, 'index']);
+Route::get('/jurusan/form/', [JurusanController::class, 'create']);
+Route::post('/jurusan/store/', [JurusanController::class, 'store']);
+Route::get('/jurusan/edit/{id}', [JurusanController::class, 'edit']);
