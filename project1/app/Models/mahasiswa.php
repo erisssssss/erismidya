e1cc7651adfa2b95872c8f6e88f1de20fd40cac6
erisssssss <168049674+erisssssss@users.noreply.github.com>
2024-06-06 00:37:48 +0700

    relasi model jurusan dan mahasiswa

--- project1/app/Models/mahasiswa.php
+++ project1/app/Models/mahasiswa.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Jurusan extends Model
+class mahasiswa extends Model
 {
     use HasFactory;
 
-    public function mahasiswas(): BelongsTo
+    public function jurusans(): HasOne
     {
-        return $this->belongsTo(Mahasiswa::class);
+        return $this->hasOne(Jurusan::class, 'id','jurusans_id');
     }
 }
